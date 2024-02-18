22000352d4feee52aff0e34e1ba7f526a3effc05
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-02-18 08:37:02 -0500

    Fix this absolute disaster

--- src/scene/animation.rs
+++ src/scene/animation.rs
@@ -15,8 +15,8 @@ pub enum Keyframes {
   Weights(Vec<f32>),
 }
 
-/// Container for raw animation data for a node (bone).
-pub struct AnimationComponent {
+/// Container containing raw TRS animation data for a node (bone).
+pub struct BoneAnimation {
   /// The name of the animation.
   pub name: String,
   /// The raw keyframe data.
