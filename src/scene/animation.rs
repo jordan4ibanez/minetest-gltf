81107a6ef76953cb4ec98c2bb2b3207beab3c674
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-02-18 05:44:20 -0500

    Stop using raw Vectors for TRS

--- src/scene/animation.rs
+++ src/scene/animation.rs
@@ -1,12 +1,14 @@
 // Based on https://whoisryosuke.com/blog/2022/importing-gltf-with-wgpu-and-rust
 // You can thank ryosuke for this information.
 
+use glam::{Quat, Vec3};
+
 /// Raw animation data.
 pub enum Keyframes {
   /// Translation raw data.
-  Translation(Vec<Vec<f32>>),
+  Translation(Vec<Vec3>),
   /// Rotation raw data.
-  Rotation(Vec<Vec<f32>>),
+  Rotation(Vec<Quat>),
   /// Scale raw data.
   Scale(Vec<Vec<f32>>),
   /// Morph Target Weights raw data.
