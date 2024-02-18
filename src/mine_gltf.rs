298b5673fee497667ef31129298d06d68411a859
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-02-18 08:42:35 -0500

    Fix this and prepare it for the future

--- src/mine_gltf.rs
+++ src/mine_gltf.rs
@@ -1,9 +1,13 @@
-use crate::{animation::AnimationClip, Scene};
+use ahash::AHashMap;
+
+use crate::{animation::BoneAnimation, Scene};
 
 ///
 /// Raw data container to hold GLTF Scene and Animation data.
 ///
 pub struct MineGLTF {
   pub scenes: Vec<Scene>,
-  pub animations: Vec<AnimationClip>,
+  // In the future: this will be an AHasMap<String, AHashMap<i32, BoneAnimation>> to support
+  // multiple animations by name.
+  pub bone_animation: AHashMap<i32, BoneAnimation>,
 }
